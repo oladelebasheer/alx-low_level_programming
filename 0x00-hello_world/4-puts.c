99c9d3f4364c4c66b8be2bd689cabac2236bf99d
oladelebasheer <ajaoooladele@gmail.com>
2022-09-08 16:11:37 -0700

    committing task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - print the string in the out function
+ *
+ * Description - the main function
+ * This program print 'Programming is like building a multilingual puzzle'
+ * Return - 0
+ */
+
+int main(void)
+{
+	put('\'"Programming is like building a multilingual puzzle");
+	return (0);
+}
