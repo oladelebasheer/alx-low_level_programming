ec38e2fcced702c18d400147c2d296dd0069ae75
oladelebasheer <ajaoooladele@gmail.com>
2022-09-08 16:24:07 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ *Main - print the script in the put function
+ *
+ */
+
+int main(void)
+{
+	printf('with proper grammar, but the outcome is a piece of art');
+	return (0);
+}
