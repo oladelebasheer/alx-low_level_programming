69ee4fc4cf780675159d3b42c2762ca02b7c028c
oladelebasheer <ajaoooladele@gmail.com>
2022-09-08 16:16:27 -0700

    committing task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description - the main function
  * This program print 'Programming is like building a multilingual puzzle'
- * Return - 0
+ * Return:0
  */
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
