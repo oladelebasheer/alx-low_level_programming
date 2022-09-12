3f92905a1d36afe0543c0395e27bc2e2e6e9a5f8
oladelebasheer <ajaoooladele@gmail.com>
2022-09-11 19:51:28 -0700

    Update task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
 /**
- *Main - print the script in the put function
+ * main - print a text
  *
+ * Reurn: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
