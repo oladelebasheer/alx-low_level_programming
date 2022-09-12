4e60a4229a7fedb3cebcc3472ccf2c3759dba8a6
oladelebasheer <ajaoooladele@gmail.com>
2022-09-11 19:36:45 -0700

    Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-/**
- *main - print the string in the out function
- * 
- * Return: 0 after printing the function.
- */
 
+/**
+ * main - print some text
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
