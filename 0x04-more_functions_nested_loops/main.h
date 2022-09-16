29fbab8e6487967ffb6bee0bdaa6378dfa409369
oladelebasheer <ajaoooladele@gmail.com>
2022-09-15 17:36:17 -0700

    Commit task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for protoypes
+ * void protoyoes (void)
+ * int protoypes (int)
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_squre(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _issupper(int);
+int mul(int, int);
+
+#endif /*MAIN_H*/
