55d7245fb840590710625b20b8109f2285cec278
oladelebasheer <ajaoooladele@gmail.com>
2022-09-16 09:04:07 -0700

    update task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i=0; i<10;i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j=0; j<15;j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j>=10)
-				_putchar(j/10+'0');
-				_putchar(j%10+'0');
+			if (j > 9)
+			{
+				putchar((j / 10) + '0');
+			}
+			putchar((j % 10) + '0');
 		}
-		_putchar('\n');
+		putchar(10);
 	}
 }
