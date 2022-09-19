893db93745c3126e4863026a56fefba1effa338f
oladelebasheer <ajaoooladele@gmail.com>
2022-09-19 16:04:42 -0700

    Committing task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @s: string
+ * Return: return length as an integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*(s _ len) != '\0')
+
+	return (len);
+}
