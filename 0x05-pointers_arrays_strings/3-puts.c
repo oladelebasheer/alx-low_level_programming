fa19c5bb625119bd676e47d4415dfb90e1486381
oladelebasheer <ajaoooladele@gmail.com>
2022-09-19 16:20:25 -0700

    Committing task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,21 +3,20 @@
 
 /**
  * _puts - print a string
- *
  * @str: string to print
- *
  * Describe: print a string
- *
  * on success - return no error
  */
 
 void _puts(char *str)
 {
 	int i = 0;
+
 	while (*(str + i) != '\0')
 	{
 		putchar(*(str + i));
 		i++
 	}
+
 	putchar (10);
 }
