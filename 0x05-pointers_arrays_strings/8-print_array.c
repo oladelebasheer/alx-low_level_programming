2b33b204cbb372dbf8f166d92bfcda8ac4d5b708
oladelebasheer <ajaoooladele@gmail.com>
2022-09-19 17:03:29 -0700

    Update task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>                           
+#include "main.h"                            
+                                             
+/**                                          
+ * puts_half - print galf of a string        
+ * @a- array                                 
+ * @n- number of elements                    
+ * return - return nothing                   
+ */                                          
+                                             
+void puts_half(char *str)                    
+{                                            
+        int inc;                             
+                                             
+        for (inc = 0; inc < n; inc++)        
+        {                                    
+                if (inc != n - 1)            
+                        printf("%d", a[inc]);
+                else                         
+                        printf("%d", a[inc]);
+        }                                    
+        putchar(10);                         
+}                                            
+~                                            
