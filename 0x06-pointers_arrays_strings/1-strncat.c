769f7f996396af843a0fff69f303b95abb578a9c
oladelebasheer <ajaoooladele@gmail.com>
2022-09-23 03:24:22 -0700

    Commit task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,21 @@
-#include "main.h"                                             
-#include <stdio.h>                                            
-                                                              
-/**                                                           
- * Strcat - concatenates two string using at most 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Strcat - concatenates two string using at most
  *
- * @dest: the pointer to be appended upon    
- * @src: the source string to be appended to @dest.           
- * Return: a pointer to the destnation string @dest.          
- */                                                           
-char *_strcat(char *dest, char *src, int n)                          
-{                                                             
-        int index = 0, dest_len = 0;                           
-        while (dest[index++])                                  
-                dest_len++;                                   
-                                                              
-        for (index = 0; src[index] && index < n; index++)                      
-                dest[dest_len++] = src[index];                 
-        return (dest);                                        
-}    
+ * @dest: the pointer to be appended upon
+ * @src: the source string to be appended to @dest.
+ * Return: a pointer to the destnation string @dest.
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+	
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
