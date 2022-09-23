989d253da010d621aa85f2751ac4e5ff67be039e
oladelebasheer <ajaoooladele@gmail.com>
2022-09-23 03:16:17 -0700

    commit task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,13 @@
  * @src: the source string to be appended to @dest.           
  * Return: a pointer to the destnation string @dest.          
  */                                                           
-char *_strcat(char *dest, char *src)                          
+char *_strcat(char *dest, char *src, int n)                          
 {                                                             
-        int indx = 0, dest_len = 0;                           
-        while (dest[indx++])                                  
+        int index = 0, dest_len = 0;                           
+        while (dest[index++])                                  
                 dest_len++;                                   
                                                               
-        for (indx = 0; src[indx] && indx < n; indx++)                      
-                dest[dest_len++] = src[indx];                 
+        for (index = 0; src[index] && index < n; index++)                      
+                dest[dest_len++] = src[index];                 
         return (dest);                                        
 }    
