4b989b82fbb0493d2b2b67219d534cf141f72292
oladelebasheer <ajaoooladele@gmail.com>
2022-09-27 08:06:34 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _memset - Fill memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to fill
- * @c: bytes of memory area to fill
+ * _memcpy - copy memory area
+ * @dest: dest memory area to fill
+ * @src: source memory area
+ * @n: bytes of memory area to fill
  *
  * Return: return the memory area filled
  */
