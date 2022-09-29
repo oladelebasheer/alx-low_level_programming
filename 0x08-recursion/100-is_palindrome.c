595fbbf78484546170de3ee658ac21331c626324
oladelebasheer <ajaoooladele@gmail.com>
2022-09-29 16:06:47 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,3 +21,15 @@ int is_palindrome(char *s)
  * @flag: flag to indicate if a string is a palindrome
  * return: void
  */
+
+void check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
