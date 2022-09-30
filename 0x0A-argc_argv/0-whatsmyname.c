e986f69094fcc72e9724149f6cf8d9ad9c9e7bbe
oladelebasheer <ajaoooladele@gmail.com>
2022-09-30 05:07:13 -0700

    Commit task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints the name of the program
  * @argc: number of command line arguments
- * @argv: araay name
+ * @argv: array name
  * return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	printf("%s\n", argv[argc * 0]);
+	printf("argc = %d\n", argv);
 
 	return (0);
 }
