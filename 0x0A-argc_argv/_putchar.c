c6e331ae0b70f2dee80144b74ee62642f0d3c145
oladelebasheer <ajaoooladele@gmail.com>
2022-10-03 09:12:40 -0700

    task 1

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,5 +1,13 @@
-#includee <unistd.h>
+#include <unistd.h>
 
 /**
  * _putchar -writes the character c to stdout
  * @c: The character to print
+ * Return: On success1.
+ * On error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
