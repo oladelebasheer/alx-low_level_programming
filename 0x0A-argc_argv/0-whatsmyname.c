2e65bcf4606b26b92b738cc5801160dc558a4fa4
oladelebasheer <ajaoooladele@gmail.com>
2022-10-04 04:06:15 -0700

    update task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,9 +12,13 @@
  * return: This return to 0.
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
 	
+	for (i = 0, i< argc; i++)
+	{	
+	printf("%s\n", argv[0]);
+	}
 	return (0);
 }
