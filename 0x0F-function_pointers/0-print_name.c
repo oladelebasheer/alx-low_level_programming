77bd9b1a88aae06ef38a3de2c2688bf78f559b95
oladelebasheer <ajaoooladele@gmail.com>
2022-10-13 03:12:09 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "FUNCTION_POINTERS_H"
+
+/**
+ * print_name -  print a name.
+ * @name: pointer to the name.
+ * @f: function pointer.
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
