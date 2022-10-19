0a71ab4542100314d0532d7e9907df152d8a99c7
oladelebasheer <ajaoooladele@gmail.com>
2022-10-19 06:48:25 -0700

    Udate task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,27 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - program that multiplies two numbers
+ * main - Prints the addition of positive numbers
+ * @argc: The number of arguments passed to the progra
+ * @argv: An array of pointers to the arguments
  *
- * @argc: argument count for main
- * @argv: vector to the arguments
- *
- * Return: void
+ * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int sum = 0;
-
-	if (argc <= 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	else if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
-		}
-	}
-	printf("%d\n", sum);
-	return (0);
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
 }
