5dc49202e90c296bda98109a371d5c0f63dcc565
oladelebasheer <ajaoooladele@gmail.com>
2022-10-19 07:05:45 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,7 +21,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char) *size);
+	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (UNLL);
