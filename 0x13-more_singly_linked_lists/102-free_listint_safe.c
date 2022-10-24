bf6b763fa468a52063814d8d0c53723e7bcc0900
oladelebasheer <ajaoooladele@gmail.com>
2022-10-24 15:12:17 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,26 +1,30 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list
- * @head: address of pointer to first node
+ * free_listint_safe - prints a list
+ * @h: address of pointer to first node
  *
  * Return: address of head
  */
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
-		}
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
 
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
+	{
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
 	}
-	return (0);
+	return (c);
 }
