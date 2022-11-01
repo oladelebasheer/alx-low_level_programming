a4f33e60dd8b47aa5f24e19fd5bb2c11290702cd
oladelebasheer <ajaoooladele@gmail.com>
2022-11-01 00:30:02 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - creates a file and puts text in it
+ * with 400 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t len = 0, inlen = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		len = write(file, text_content, inlen);
+	}
+
+	if (close(file) == -1 || inlen != len)
+		return (-1);
+	return (1);
+}
